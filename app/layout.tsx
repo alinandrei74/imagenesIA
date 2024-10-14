7ffc92064f381decb38550665d04a9cc3b761f5a
entremeses <alinandrei74@gmail.com>
2024-10-14 12:32:13 +0200

    cambios en el inicio de sesión

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,19 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans} from "next/font/google";
-import "./globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
-}
-)
-
+});
 
 export const metadata: Metadata = {
-  title: "conversor",
-  description: "tratado de imagenes con ia",
+  title: "Imaginify",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -30,20 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      layout: {
-        unsafe_disableDevelopmentModeWarnings: true
-      },
-      variables: { colorPrimary: "#624cf5"}
-      
+      variables: { colorPrimary: '#624cf5' }
     }}>
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            
-          </SignedIn>
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
         </body>
       </html>
